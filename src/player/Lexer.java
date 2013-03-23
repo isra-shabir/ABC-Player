970f1bfabedf6b140540cda51860140045c3ab64
Abdulaziz Alghunaim <azizkag@gmail.com>
2013-03-23 15:58:26 -0400

    lexer skeleton

--- src/player/Lexer.java
+++ src/player/Lexer.java
@@ -45,12 +45,42 @@ public class Lexer {
     	Matcher matcher = patterns.matcher(this.stream);
     	
     	//define the different patterns from the tokens
-    	Pattern var = Pattern.compile(Type.BASENOTE.getRegex());
-    	Pattern num = Pattern.compile(Type.NUM.regex);
-    	Pattern mul = Pattern.compile(Type.MUL.regex);
-    	Pattern add = Pattern.compile(Type.ADD.regex);
-    	Pattern lb = Pattern.compile(Type.LB.regex);
-    	Pattern rb = Pattern.compile(Type.RB.regex);
+    	Pattern nume = Pattern.compile(Type.NUMERATOR.getRegex());
+    	Pattern deno = Pattern.compile(Type.DENOMINATOR.getRegex());
+    	Pattern note = Pattern.compile(Type.BASENOTE.getRegex());
+    	Pattern acc = Pattern.compile(Type.ACCIDENTAL.getRegex());
+    	Pattern octave = Pattern.compile(Type.OCTAVE.getRegex());
+    	Pattern len = Pattern.compile(Type.LENGTH.getRegex());
+    	Pattern choBeg = Pattern.compile(Type.CHORDBEGIN.getRegex());
+    	Pattern choEnd = Pattern.compile(Type.CHORDEND.getRegex());
+    	Pattern tup = Pattern.compile(Type.TUPLET.getRegex());
+    	Pattern bar = Pattern.compile(Type.BAR.getRegex());
+    	Pattern repeat = Pattern.compile(Type.REPEATBEG.getRegex());
+    	Pattern voice = Pattern.compile(Type.VOICE.getRegex());
+    	Pattern endBar = Pattern.compile(Type.ENDBAR.getRegex());
+    	Pattern space = Pattern.compile(Type.SPACE.getRegex());
+    	
+    	
+    	
+    	while (matcher.find()){
+    		
+    		String token = matcher.group();
+    		
+    		if(nume.matcher(token).matches())		tokens.add(new Token(Type.NUMERATOR, token));
+    		else if(deno.matcher(token).matches())		tokens.add(new Token(Type.DENOMINATOR, token));
+    		else if(note.matcher(token).matches())		tokens.add(new Token(Type.BASENOTE, token));
+    		else if(acc.matcher(token).matches())		tokens.add(new Token(Type.ACCIDENTAL, token));
+    		else if(octave.matcher(token).matches())		tokens.add(new Token(Type.OCTAVE, token));
+    		else if(len.matcher(token).matches())		tokens.add(new Token(Type.LENGTH, token));
+    		else if(choBeg.matcher(token).matches())		tokens.add(new Token(Type.CHORDBEGIN, token));
+    		else if(choEnd.matcher(token).matches())		tokens.add(new Token(Type.CHORDEND, token));
+    		else if(tup.matcher(token).matches())		tokens.add(new Token(Type.TUPLET, token));
+    		else if(bar.matcher(token).matches())		tokens.add(new Token(Type.BAR, token));
+    		else if(repeat.matcher(token).matches())		tokens.add(new Token(Type.REPEATBEG, token));
+    		else if(voice.matcher(token).matches())		tokens.add(new Token(Type.VOICE, token));
+    		else if(endBar.matcher(token).matches())		tokens.add(new Token(Type.ENDBAR, token));
+    		else if(space.matcher(token).matches())		tokens.add(new Token(Type.SPACE, token));
+    	}
 		return tokens;
 	}
 }
