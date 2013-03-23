d27ce959d847ca42dbf2cdb34c95a3e2cf305a93
Abdulaziz Alghunaim <azizkag@gmail.com>
2013-03-23 16:23:30 -0400

    lexer test

---
+++ src/player/LexerTest.java
@@ -0,0 +1,30 @@
+package player;
+
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import player.Token.Type;
+
+
+public class LexerTest {
+	
+    @Test
+    public void LexerTest1() {
+    	//test basic functionality for basenote 	
+    	Lexer test = new Lexer("AB");
+    	
+    	ArrayList<Token> expected = new ArrayList<Token>();
+    	
+    			
+    	expected.add(new Token(Type.BASENOTE, "A"));
+    	expected.add(new Token(Type.BASENOTE, "B"));
+
+    	assertEquals(expected.toString(), test.lex().toString());     		
+    
+    }
+
+}
