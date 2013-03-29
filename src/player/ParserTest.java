b32864813f6828f67ae508023a808490b4dc3636
Abdulaziz Alghunaim <azizkag@gmail.com>
2013-03-29 14:38:16 -0400

    parser beta

--- src/player/ParserTest.java
+++ src/player/ParserTest.java
@@ -12,7 +12,60 @@ public class ParserTest {
 	public void ParserTest1(){
 		//test note constructor with only a note
 		
+		Lexer test = new Lexer("AB");
+		Parser parse = new Parser(test.lex());
 		
+		String output = (parse.parse().toString());
+		System.out.println(output);
+		assertEquals(output,"[ A ,  B ]");
+	}
+	
+	@Test
+	public void ParserTest2(){
+		//test note constructor with notes and accidental
+		
+		Lexer test = new Lexer("_A ^B");
+		Parser parse = new Parser(test.lex());
+		
+		String output = (parse.parse().toString());
+		System.out.println(output);
+		assertEquals(output,"[_ A , ^ B ]");
+	}
+	
+	@Test
+	public void ParserTest3(){
+		//test note constructor with notes and lengths
+		
+		Lexer test = new Lexer("_A2/3");
+		Parser parse = new Parser(test.lex());
+		
+		String output = (parse.parse().toString());
+		System.out.println(output);
+		assertEquals(output,"[_ A , ^ B ]");
+	}
+	
+	@Test
+	public void ParserTest4(){
+		//test note constructor with notes and lengths (no den)
+		
+		Lexer test = new Lexer("_A/");
+		Parser parse = new Parser(test.lex());
+		
+		String output = (parse.parse().toString());
+		System.out.println(output);
+		assertEquals(output,"[_ A , ^ B ]");
+	}
+	
+	@Test
+	public void ParserTest5(){
+		//test note constructor with notes and lengths (no den)
+		
+		Lexer test = new Lexer("_A/5");
+		Parser parse = new Parser(test.lex());
+		
+		String output = (parse.parse().toString());
+		System.out.println(output);
+		assertEquals(output,"[_ A , ^ B ]");
 	}
 	
 	
